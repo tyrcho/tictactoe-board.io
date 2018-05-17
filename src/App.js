d183b51cdfd7d4b3e82dd91052371c58b248751a
Michel Daviot <michel@daviot.info>
2018-05-17 18:10:04 +0200

    :sparkles: finished TicTacToe implementation with winner detection

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Client} from 'boardgame.io/react';
+import {Game} from 'boardgame.io/core';
+import {TicTacToeBoard} from './TicTacToeBoard';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+const TicTacToe = Game({
+    setup: () => ({cells: Array(9).fill(null)}),
+
+    moves: {
+        clickCell(G, ctx, id) {
+            const cells = [...G.cells];
+
+            // Ensure we can't overwrite cells.
+            if (cells[id] === null) {
+                cells[id] = ctx.currentPlayer;
+            }
+
+            return {...G, cells};
+        },
+    },
+
+    flow: {
+        endGameIf: (G, ctx) => {
+            if (IsVictory(G.cells)) {
+                return ctx.currentPlayer;
+            }
+        },
+    },
+});
+
+/**
+ * @return {boolean}
+ */
+export function IsVictory(cells) {
+    let linesIndices = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
+    let colIndices = [[0, 3, 6], [1, 4, 7], [2, 5, 8]];
+    let diagIndices = [[0, 4, 8], [2, 4, 6]];
+    let winningIndices = [linesIndices, colIndices, diagIndices];
+    return ["0", "1"].some(player =>
+        winningIndices.some(indices =>
+            indices.some(line =>
+                line.every(index =>
+                    cells[index] === player))));
 }
 
-export default App;
+export const App = Client({
+    game: TicTacToe,
+    board: TicTacToeBoard,
+});
+
+
+
+
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {App, IsVictory} from './App';
+
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+    const div = document.createElement('div');
+    ReactDOM.render(<App/>, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
+
+test('winner with all empty cells', () => {
+    expect(IsVictory(Array(9).fill(null))).toBe(false);
+});
+
+
+test('winner with full cells', () => {
+    expect(IsVictory(Array(9).fill("1"))).toBe(true);
+});
+
+test('winner with 3 cells in line', () => {
+    expect(IsVictory(["0", "0", "0", null, null, null, null, null, null])).toBe(true);
+});
+
+test('winner with 3 cells in col', () => {
+    expect(IsVictory([
+        "0", null, "0",
+        "0", null, null,
+        "0", null, null])).toBe(true);
+});
+
+test('winner with 3 cells in diag', () => {
+    expect(IsVictory([
+        "0", null, "0",
+        "1", "0", null,
+        "0", null, null])).toBe(true);
+});
+
+test('no winner with 3 cells', () => {
+    expect(IsVictory([null, 0, 0, 0, null, null, null, null, null])).toBe(false);
 });
